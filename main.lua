b5244d6fe461a014258745450afdf8594e190c26
Hawdie <maniak.kb@gmail.com>
2025-06-24 13:52:36 +0200

    Pacman Game edited.

--- main.lua
+++ main.lua
@@ -1,15 +1,63 @@
 _G.love = require("love")
 
 function love.load()
-    _G.number = 0
+    love.graphics.setBackgroundColor(0.5, 0.5 , 1)
+
+    _G.pacman = {};
+    pacman.x = 200;
+    pacman.y = 250;
+    pacman.angle1 = 1;
+    pacman.angle2 = 5;
+    
+    _G.food ={
+        x = 600;
+        eaten = false;
+    };
 end
 
 
 function love.update(dt)
-    number = dt
+
+    if love.keyboard.isDown("down") then
+        pacman.angle1 = pacman.angle1 - math.pi * dt;
+        pacman.angle2 = pacman.angle2 - math.pi * dt;
+    elseif love.keyboard.isDown("up")then
+        pacman.angle1 = pacman.angle1 + math.pi * dt;
+        pacman.angle2 = pacman.angle2 + math.pi * dt;
+    end
+
+    --*Pacman movement 4 if statements
+    if love.keyboard.isDown("a") then
+        pacman.x = pacman.x - 1;
+    end
+
+    if love.keyboard.isDown("d") then
+        pacman.x = pacman.x + 1;
+    end
+    if love.keyboard.isDown("s") then
+        pacman.y = pacman.y + 1;
+    end
+    if love.keyboard.isDown("w") then
+        pacman.y = pacman.y - 1;
+    end
+
+    if pacman.x >= food.x + 20 then
+        food.eaten = true;
+    end
 end
 
 
 function love.draw()
-    love.graphics.print(number)
+    if not food.eaten  then
+        
+    love.graphics.setColor(0, 0, 0)
+    love.graphics.rectangle("fill", 600, 200, 100, 90)
+    
+    end
+
+    love.graphics.setColor( 1, 1, 1)
+    love.graphics.arc("fill", pacman.x, pacman.y, 60, pacman.angle1, pacman.angle2)
+
+
+
 end
